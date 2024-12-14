882d16e3c6a5b13b201790d897caadcc6869992b
ThisaraniAloka <164716628+ThisaraniAloka@users.noreply.github.com>
2024-12-14 18:37:32 +0530

    nav-change

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -8,8 +8,7 @@ const Navbar = () => {
         <li>Home</li>
         <li>Products</li>
         <li>About</li>
-        <li>Contact 1</li>
-
+        <li>Contact</li>
       </ul>
     </div>
     
